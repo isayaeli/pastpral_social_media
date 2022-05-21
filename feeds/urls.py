5d7daf4916d4d481b9f682e2275a086d40a47728
isayaeli <isayaelib@gmail.com>
2022-05-21 15:27:55 +0300

    feed updates

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.feeds, name='feeds' )
+    path('', views.feeds, name='feeds' ),
+    path('delete-post/<int:id>', views.delete_feed, name='delete_feed')
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Feed
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 @login_required(login_url='/login')
 def feeds(request):
     feeds = Feed.objects.all().order_by('-id')
+    if request.method == 'POST':
+        print(request.POST)
+        feed = Feed()
+        feed.user = request.user
+        feed.texts = request.POST['texts']
+        feed.image = request.FILES.get('image')
+        feed.video = request.FILES.get('video')
+        feed.save()
+
+        return redirect('feeds')
     context = {
         'feeds':feeds
     }
     return render(request, 'feeds/feeds.html',context)
+
+def delete_feed(request, id):
+    feed =  get_object_or_404(Feed, id=id)
+    feed.delete()
+    messages.success(request, 'Post has been deleted!')
+    return redirect('feeds')
