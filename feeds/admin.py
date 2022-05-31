7664472bf6e8f6842de21e402a933d2dcc15200e
isayaeli <isayaelib@gmail.com>
2022-05-31 18:33:28 +0300

    adding comment section

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,8 +1,10 @@
+
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import Feed
+from .models import Feed,Comment
 # Register your models here.
 admin.site.register(Feed)
+admin.site.register(Comment)
 
 admin.site.unregister(Group)
--- feeds/models.py
+++ feeds/models.py
@@ -14,6 +14,11 @@ class Feed(models.Model):
     def __str__(self) :
         return str(self.user)
 
+    @property
+    def get_comment(self):
+        comments = Comment.objects.filter(feed_id=self.id)
+        return comments
+
 
     @property
     def published(self):
@@ -65,3 +70,60 @@ class Feed(models.Model):
                 
                 
                 
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return str(self.user)
+
+
+    @property
+    def published(self):
+        now = timezone.now()
+
+        diff = now - self.created_on
+        if diff.days == 0 and diff.seconds >= 0 and diff.seconds < 60:
+            seconds = diff.seconds
+            if seconds == 1:
+                return str(seconds) + " second ago"
+            else:
+                return str(seconds) + " seconds ago"
+
+        if diff.days == 0 and diff.seconds >= 60 and diff.seconds < 3600:
+            minutes = math.floor(diff.seconds/60)
+            if minutes == 1:
+                return str(minutes) + " minute ago"
+            else:
+                return str(minutes) + " minutes ago"
+
+        if diff.days == 0 and diff.seconds >= 3600 and diff.seconds < 86400:
+            hours = math.floor(diff.seconds/3600)
+            if hours == 1:
+                return str(hours) + " hour ago"
+            else:
+                return str(hours) + " hours ago"
+
+        if diff.days >= 1 and diff.days < 30:
+            days = diff.days
+            if days == 1:
+                return str(days) + " day ago"
+            else:
+                return str(days) + " days ago"
+
+        if diff.days >= 30 and diff.days < 365:
+            months = math.floor(diff.days/30)
+            if months == 1:
+                return str(months) + " month ago"
+                
+            else:
+                return str(months) + " months ago"
+
+        if diff.days >= 365:
+            years = math.floor(diff.days/365)
+            if years == 1:
+                return str(years) + " year ago"
+            else:
+                return str(years) + " years ago"
